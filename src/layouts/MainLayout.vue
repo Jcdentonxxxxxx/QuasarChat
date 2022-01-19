4fb815669b1cff6a9ac7d4cdd87952f586fcf306
italik <vitalik@email.com>
2022-01-19 17:42:00 +0300

    Create app

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,43 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header elevated>
+      <q-toolbar>
+        
+
+        <q-toolbar-title class="absolute-center">
+          {{title}}
+        </q-toolbar-title>
+
+        
+      </q-toolbar>
+    </q-header>
+
+   
+   
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+import { defineComponent, ref } from 'vue'
+
+export default defineComponent({
+  computed: {
+    title() {
+      console.log(this.$route.fullPath);
+      let currentPath = this.$route.fullPath;
+
+      if (currentPath === '/') {
+        return 'QuasarChat';
+      } else if(currentPath === '/chat') {
+        return 'Chat';
+      } else if(currentPath === '/auth') {
+        return 'Login';
+      }
+    }
+  }
+})
+</script>
